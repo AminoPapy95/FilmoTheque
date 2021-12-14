f4126c9e2809fc70f4ea47d3b9eef49fdaaae120
Amine HOUSSANE <aminehoussane@gmail.com>
2021-12-14 13:27:36 +0100

    initialization

---
+++ resources/views/master.blade.php
@@ -0,0 +1,30 @@
+<html lang="fr">
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <title>Inscrire un titre ici</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}" >
+
+    <title>Hello, world!</title>
+</head>
+<body>
+
+<div class="wrapper">
+
+@include('body.header')
+
+</div>
+
+
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<!-- FONT AWESOME LAST VERSION -->
+<script src="https://kit.fontawesome.com/4a0becc8fb.js" crossorigin="anonymous"></script>
+</body>
+</html>
