239b0a66e8d9b74b99805f592716e2b83936d08b
Amine HOUSSANE <aminehoussane@gmail.com>
2022-02-15 15:42:58 +0100

    Models, controllers, templates, routes for Type, Locality and Role

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\LocalityController;
+use App\Http\Controllers\RoleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +22,22 @@ Route::get('/', function () {
     return view('master');
 });
 
+// ROUTE ARTIST
 Route::get('/artist', [ArtistController::class, 'index'])->name('artist_index');
 
 Route::get('/artist/{id}', [ArtistController::class, 'show'])->where('id', '[0-9]+')->name('artist_show');
+
+//ROUTE TYPE
+Route::get('/type', [TypeController::class, 'index'])->name('type_index');
+Route::get('/type/{id}', [TypeController::class, 'show'])
+		->where('id', '[0-9]+')->name('type_show');
+
+//ROUTE LOCALITY
+Route::get('/locality', [LocalityController::class, 'index']) ->name('locality_index');
+Route::get('/locality/{id}', [LocalityController::class, 'show'])
+		->where('id', '[0-9]+')->name('locality_show');
+
+//ROUTE ROLE
+Route::get('/role', [RoleController::class, 'index'])->name('role_index');
+Route::get('/role/{id}', [RoleController::class, 'show'])
+		->where('id', '[0-9]+')->name('role_show');
