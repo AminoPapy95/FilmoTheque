39e7c5b617e1df64bbf8e2fb9d9374bf4d7c1258
Amine HOUSSANE <aminehoussane@gmail.com>
2022-02-17 13:50:50 +0100

    Update templates Artist & Type

--- resources/views/type/show.blade.php
+++ resources/views/type/show.blade.php
@@ -5,4 +5,12 @@
 @section('content')
     <h1>{{ ucfirst($type->type) }}</h1>
     <nav><a href="{{ route('type_index') }}">Retour à l'index</a></nav>
+
+    <h2>Liste des artistes</h2>
+    <ul>
+    @foreach($type->artists as $artist)
+        <li>{{ $artist->firstname }} {{ $artist->lastname }}</li>
+    @endforeach
+    </ul>
+
 @endsection
