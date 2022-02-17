5f3bb70b7438a0f22978cac8d76304f8aa9f90a0
Amine HOUSSANE <aminehoussane@gmail.com>
2022-02-17 13:44:31 +0100

    Relation ManyToMany between Artist & Type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             LocationSeeder::class,
             ShowSeeder::class,
             RepresentationSeeder::class,
+            ArtistTypeSeeder::class,
         ]);
 
     }
